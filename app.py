d3f26193f095d82e820ad06651e954b9c86b85cd
Rajeev Nethran <rajeevnethran.arun@gmail.com>
2026-09-05 04:40:20 +0530

    Change page icon to static/icon-192.png

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ import streamlit.components.v1 as components
 
 st.set_page_config(
     page_title = "BBC AI",
-    page_icon = "🐄",
+    page_icon = "static/icon-192.png",
     layout = "wide",
 )
 
